e06282e66bda6fc89352358ee258f6d5a6a3c91b
IL <irewlg@outlook.com>
2025-07-20 23:55:34 -0400

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router"
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link to="/" className="logo">
+        <img src="" alt="" />
+        <span>WriteNode</span>
+      </Link>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <button className="auth">Login</button>
+      </nav>
+    </header>
+
+    
   )
 }
