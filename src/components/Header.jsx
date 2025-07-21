d9a10b83035d835a2dc89009fb5cf75a3227ce46
IL <irewlg@outlook.com>
2025-07-21 01:12:00 -0400

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router"
+import { Link, NavLink} from "react-router"
+import Logo from "../assets/logo.png"
 
 export const Header = () => {
+  const isAuth = true;
+
   return (
     <header>
       <Link to="/" className="logo">
-        <img src="" alt="" />
+        <img src={Logo} alt="WriteNode Logo" />
         <span>WriteNode</span>
       </Link>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <button className="auth">Login</button>
+        <NavLink to="/" className="link">Home</NavLink>
+        { isAuth ? (
+          <>
+          <NavLink to="/create" className="link">Create</NavLink> 
+          <button className="auth"><i className="bi bi-box-arrow-right"></i>Logout</button> 
+          </>
+        ) : (
+        <button className="auth"><i className="bi bi-google"></i>Login</button>
+        ) }
+       
       </nav>
     </header>
 
