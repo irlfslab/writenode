3df8a2058852e383a2f4fbc7beb32de606b6eba2
IL <irewlg@outlook.com>
2025-07-26 23:56:20 -0400

    updated

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,9 @@
+import PageNotFound from "../assets/images/page-not-found.jpg"
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <section className="pageNotFound">
+      <p>404 / Page Not found</p>
+      <img src={PageNotFound} alt="Page Not Found" />
+    </section>
   )
 }
