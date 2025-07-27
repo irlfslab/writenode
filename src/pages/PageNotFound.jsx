511dfdfa1ed1c16fc59a83e8998f0a1f545468b2
IL <irewlg@outlook.com>
2025-07-27 00:06:57 -0400

    updated

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,14 @@
-import PageNotFound from "../assets/images/page-not-found.jpg"
+import {Link} from "react-router";
+import NotFound from "../assets/page-not-found.jpg";
+
 export const PageNotFound = () => {
   return (
     <section className="pageNotFound">
       <p>404 / Page Not found</p>
-      <img src={PageNotFound} alt="Page Not Found" />
+      <img src={NotFound} alt="Page Not Found" />
+      <Link to ="/">
+        <button>Back to Home</button>
+      </Link>
     </section>
   )
 }
