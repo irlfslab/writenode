25cac58df24d12eaa6067358da1642b75ed99f4a
IL <irewlg@outlook.com>
2025-08-04 23:35:56 -0400

    updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase/config"
 import { PostCard } from "../components";
 
 export const HomePage = () => {
+  /** 
   const posts = [
     {id: 1, title: "Lorem ipsum dolor sit amet.", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, illum.", author: "User1"},
     {id: 2, title: "Lorem ipsum dolor", description: "Lorem ipsum dolor sit, amet consectetur", author: "User1"}
   ]
+    **/
+   const [posts, setPosts] = useState([]);
+   const postsRef = collection(db, "posts");
+   useEffect(() => {
+      async function getPosts() {
+        const data = await getDocs(postsRef);
+        console.log(data.docs);
+        setPosts(data.docs.map((document) => (
+          {...document.data(), id: document.id})));
+      }
+      console.log("---")
+      getPosts(); 
+   },[]);
+
   return (
     <section>
       { posts.map(post => (
